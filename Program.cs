2a1ce2ede944b5cbb00f541483ac4107dc471c53
roncalw <roncalw@hotmail.com>
2023-01-06 20:51:39 -0500

    Production Version 1.0.0

--- Program.cs
+++ Program.cs
@@ -14,19 +14,50 @@ builder.Services.AddScoped<IBidRepository, BidRepository>();
 
 var app = builder.Build();
 
+
+app.Use(async (context, next) =>
+{
+    var methodvalue = context.Request.Method;
+    if (!string.IsNullOrEmpty(methodvalue))
+    {
+ 
+        if (methodvalue == HttpMethods.Options || methodvalue == HttpMethods.Head)
+        {
+            await context.Response.WriteAsync("Option Request");
+        }
+        else
+        {
+            await next();
+        }
+    }
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI();    
+    app.UseCors(p => p.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod().AllowCredentials());
 }
 
-app.UseCors(p => p.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod().AllowCredentials());
 
 app.UseHttpsRedirection();
 
 app.MapHouseEndpoints();
 app.MapBidEndpoints();
+
+/*
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == "OPTIONS")
+            {
+                context.Response.StatusCode = 200;
+                await context.Response.WriteAsync("Ok");
+            }
+    
+    await next.Invoke();
+});
+*/
 
 
 app.Run();
--- data/HouseDbContext.cs
+++ data/HouseDbContext.cs
@@ -1,19 +1,30 @@
 public class HouseDbContext : DbContext
 {
-  public HouseDbContext(DbContextOptions<HouseDbContext> options) : base(options) { }
-  public DbSet<HouseEntity> Houses => Set<HouseEntity>();
-  public DbSet<BidEntity> Bids => Set<BidEntity>();
-  
-  protected override void OnConfiguring(DbContextOptionsBuilder options)
-  {
-      var folder = Environment.SpecialFolder.LocalApplicationData;
-      var path = Environment.GetFolderPath(folder);
-      options.UseSqlite($"Data Source={Path.Join(path, "houses1.db")}");
-  }
 
-  protected override void OnModelCreating(ModelBuilder modelBuilder)
-  {
-      SeedData.Seed(modelBuilder);
-  }
+
+    protected readonly IConfiguration Configuration;
+
+    public HouseDbContext(DbContextOptions<HouseDbContext> options, IConfiguration configuration) : base(options) 
+    { 
+        Configuration = configuration; 
+    }
+    public DbSet<HouseEntity> Houses => Set<HouseEntity>();
+    public DbSet<BidEntity> Bids => Set<BidEntity>();
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        //The following stores the database at the following location: C:\Users\Carlo\AppData\Local
+        //var folder = Environment.SpecialFolder.LocalApplicationData;
+        //var path = Environment.GetFolderPath(folder);
+        //options.UseSqlite($"Data Source={Path.Join(path, "houses1.db")}");
+
+        //The following uses the appsettings.json file to make it configurable
+        options.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        SeedData.Seed(modelBuilder);
+    }
 
 }
